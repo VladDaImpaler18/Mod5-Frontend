3848611941c46cc1bb06c7f197b4504ed558a19f
Vlad <VladDaImpaler@gmail.com>
2021-01-21 20:02:08 -0500

    reserved coresponding for forms; no data yet

---
+++ src/components/budgetForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+//budget_items( 
+//    id: uuid, name: text, amount:money, reccuring:boolean
+//    expirationDate:DATE
+//    user_id: uuid, wishlist_item_id: uuid, created_at: datetime
+//    )
+class budgetForm extends Component {
+    state = {
+        text: ''
+    }
+    
+    handleOnChange = event => null; //do this
+
+    handleOnSubmit = event => null; //do this
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/wishlistForm.js
@@ -0,0 +1,4 @@
+//wishlist_items(
+//    id: uuid, name: text, description:text, price:money, url:text,
+//    priority:integer, user_id:uuid, created_at:datetime
+//    )
